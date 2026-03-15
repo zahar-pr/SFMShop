ec396138393b155c43283e55e15448789c95fefa
zahar-pr <jedai.in.it@gmail.com>
2026-03-15 17:10:36 +0500

    add handlers exceptions

--- database/connection.py
+++ database/connection.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import Error
 
 
 class PostgresConnection:
@@ -9,13 +10,18 @@ class PostgresConnection:
         self.password = password
 
     def __enter__(self):
-        self.conn = psycopg2.connect(
-            host=self.host,
-            database=self.database,
-            user=self.user,
-            password=self.password,
-        )
-        return self.conn
+        try:
+            self.conn = psycopg2.connect(
+                host=self.host,
+                database=self.database,
+                user=self.user,
+                password=self.password,
+            )
+            return self.conn
+        except Error as e:
+            print(f"Ошибка подключения к БД: {e}")
+            return None
+
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type:
@@ -27,74 +33,81 @@ class PostgresConnection:
 
 
 def add_product(conn, name, price, quantity):
-    with conn.cursor() as cursor:
-        cursor.execute(f"INSERT INTO products (name, price, quantity) VALUES {(name, price, quantity)}")
-    print(f"Товар добавлен: {name}, {price}, {quantity}")
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                INSERT INTO products (name, price, quantity) VALUES (%s, %s, %s)
+            """, (name, price, quantity,))
+        print(f"Товар добавлен: {name}, {price}, {quantity}")
+    except Error as e:
+        print(f"Ошибка при добавлении товара: {e}")
+        return None
 
 
 def get_all_products(conn):
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT * FROM products")
-        products = cursor.fetchall()
-    return products
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM products")
+            products = cursor.fetchall()
+        return products
+    except Error as e:
+        print(f"Ошибка при получении товаров: {e}")
+        return None
 
 
 def update_product_price(conn, product_id, new_price):
-    with conn.cursor() as cursor:
-        cursor.execute(f"UPDATE products SET price = {new_price} WHERE id = {product_id}")
-    print(f"Цена обновлена: {new_price}")
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                UPDATE products SET price = %s WHERE id = %s
+            """, (new_price, product_id,))
+        print(f"Цена обновлена: {new_price}")
+    except Error as e:
+        print(f"Ошибка при обновлении цены товара: {e}")
+        return None
 
 
 def get_user_by_id(conn, user_id) -> dict | None:
-    with conn.cursor() as cursor:
-        cursor.execute("""
-            SELECT *
-            FROM users
-            WHERE id = %s
-        """, (user_id,))
-        result = cursor.fetchall()
-        return result if result != [] else ValueError(f"Пользователь с id = {user_id} не найден")
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                SELECT *
+                FROM users
+                WHERE id = %s
+            """, (user_id,))
+            result = cursor.fetchall()
+            return result if result != [] else ValueError(f"Пользователь с id = {user_id} не найден")
+    except Error as e:
+        print(f"Ошибка при получении пользователя: {e}")
+        return None
 
 
 def delete_order(conn, order_id):
-    with conn.cursor() as cursor:
-        cursor.execute("""
-            DELETE FROM orders
-            WHERE id = %s
-        """, (order_id,))
-        result = cursor.fetchall()
-        return result if result != [] else ValueError(f"Заказ с id = {order_id} не найден")
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                DELETE FROM orders
+                WHERE id = %s
+            """, (order_id,))
+            result = cursor.fetchall()
+            return result if result != [] else ValueError(f"Заказ с id = {order_id} не найден")
+    except Error as e:
+        print(f"Ошибка при удалении заказа: {e}")
+        return None
 
 
 def main():
-    with PostgresConnection("localhost", "sfmshop", "postgres", "postgres") as conn:
-        print(get_user_by_id(conn, 100))
-        delete_order(conn, 2)
-
+    try:
+        with PostgresConnection("localhost", "sfmshop", "postgres", "postgres") as conn:
+            print(get_user_by_id(conn, 100))
+            delete_order(conn, 2)
+    except Error as e:
+        print(f"Ошибка подключения к БД: {e}")
+        return None
 
 
 if __name__ == "__main__":
     main()
-
-
-"""
-Товар добавлен: Ноутбук, 50000, 10
-(2, 'Мышь', Decimal('1500.00'), 20)
-(4, 'Ноутбук', Decimal('50000.00'), 10)
-(5, 'Ноутбук', Decimal('1000000.00'), 10)
-(6, 'Ноутбук', Decimal('1000000.00'), 10)
-(1, 'Ноутбук', Decimal('45000.00'), 10)
-(7, 'Ноутбук', Decimal('50000.00'), 10)
-Цена обновлена: 45000
-
-Process finished with exit code 0
-"""
-
-
-
-
-
-
 
 
 
